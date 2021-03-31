cd54388071d55ff1be6b202cecaf97c17ce24a20
cbarrie6 <chrisjbarrie@googlemail.com>
2021-03-31 10:34:58 +0300

    Commit supplementary internat. soc analysis

--- 04_supp_analysis.R
+++ 04_supp_analysis.R
@@ -6,8 +6,16 @@ library(ggthemes)
 ################
 load("data/analysis/bib_records_supp.RData")
 
+journals <- unique(M_all_supp$SO)
+pjournals <- journals[c(5, 9)]
+npjournals <- setdiff(journals, pjournals)
+
+
+# Moby and SMS
+
 tidy_kwds <- M_all_supp %>% 
   select(AU, TI, SO, PY, ID) %>%
+  filter(SO %in% pjournals) %>%
   mutate(kwd = tolower(ID)) %>%
   unnest_tokens(word, kwd, token = stringr::str_split, pattern = "; ")
 
@@ -36,3 +44,86 @@ ggplot(yr_term_counts, aes(year, sum_out / year_total, group=1)) +
   scale_y_continuous(labels = scales::percent_format(),
                      expand = c(0, 0), limits = c(0, NA)) +
   theme_tufte(base_family = "Helvetica")
+
+
+# International soc.
+
+pwords <-
+  c(
+    "protest",
+    "protestation",
+    "protesta",
+    "protesta",
+    "protest",
+    "protests",
+    "protestations",
+    "proteste",
+    "protestas",
+    "proteste",
+    "protestor",
+    "protestataire",
+    "manifestante",
+    "manifestante",
+    "demonstrantin",
+    "protestors",
+    "protestataires",
+    "manifestanti",
+    "manifestantes",
+    "demonstrantinnen",
+    "social movement",
+    "mouvement social",
+    "movimento sociale",
+    "movimiento social",
+    "soziale bewegung",
+    "social movements",
+    "mouvements sociaux",
+    "movimenti sociali",
+    "movimientos sociales",
+    "soziale bewegungen",
+    "contentious politics",
+    "política contenciosa"
+  )
+pwords <- paste0("\\b", pwords, "\\b",  collapse = "|")
+
+
+M <- M_all_supp %>%
+  filter(!is.na(AB),!is.na(PY), 
+         SO %in% npjournals)
+
+tidy_abs <- M %>%
+  unnest_tokens(ngram, AB, token = "ngrams", n = 2)
+
+term_counts <- tidy_abs %>%
+  dplyr::group_by(PY) %>%
+  dplyr::count(ngram, sort = TRUE)
+
+term_counts$pword <- as.integer(grepl(pwords, 
+                                      x = term_counts$ngram, ignore.case = T))
+
+yr_term_counts <- term_counts %>%
+  extract(PY, "year", convert = TRUE) %>%
+  complete(year, ngram, fill = list(n = 0)) %>%
+  dplyr::group_by(year) %>%
+  dplyr::mutate(year_total = sum(n)) %>%
+  dplyr::filter(pword==1) %>%
+  dplyr::summarise(sum_p = sum(n),
+                   year_total= min(year_total)) %>%
+  na.omit()
+
+yr_term_counts %>%
+  filter(year <= 2020) %>%
+  ggplot(aes(year, (sum_p / year_total) * 100)) +
+  geom_point(alpha = 0.8) +
+  geom_smooth(
+    method = "lm",
+    linetype = "dashed",
+    color = "red",
+    alpha = .15
+  ) +
+  ylim(0, .25) +
+  xlab("Year") +
+  ylab("% protest words in journal abstracts") +
+  ggtitle("") +
+  theme_tufte(base_family = "Helvetica") +
+  theme(axis.text = element_text(size = 15),
+        axis.title = element_text(size = 15))
